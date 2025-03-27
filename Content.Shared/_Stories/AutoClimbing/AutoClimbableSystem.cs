5e8c1ef7da2b83cc7e2a57fb0742b1e01deaa91f
Inconnu1337 <elasbus@gmail.com>
2025-03-27 22:10:54 +0200

    fixed autoclimb to dead or entities who have a small mob layer

--- Content.Shared/_Stories/AutoClimbing/AutoClimbableSystem.cs
+++ Content.Shared/_Stories/AutoClimbing/AutoClimbableSystem.cs
@@ -1,5 +1,6 @@
 using Content.Shared.Climbing.Components;
 using Content.Shared.Climbing.Systems;
+using Content.Shared.Mobs.Systems;
 using Robust.Shared.Configuration;
 using Robust.Shared.Physics.Events;
 using Robust.Shared.Player;
@@ -10,7 +11,7 @@ public sealed class AutoClimbableSystem : EntitySystem
 {
     [Dependency] private readonly ClimbSystem _climb = default!;
     [Dependency] private readonly INetConfigurationManager _netConfig = default!;
-
+    [Dependency] private readonly MobStateSystem _mobState = default!;
     private bool _collide = false;
     public override void Initialize()
     {
@@ -30,6 +31,9 @@ public sealed class AutoClimbableSystem : EntitySystem
         {
             return;
         }
+
+        if (HasComp<AutoClimbBlockedComponent>(args.OtherEntity) || _mobState.IsIncapacitated(args.OtherEntity))
+            return;
 
         if (!HasComp<AutoClimbableComponent>(ent) || !TryComp<ClimbableComponent>(ent, out var climb))
             return;
