750a8950f3fb4a001c07b167fbea76d6b51e0980
ZeD11133 <kirillkhannanov2017@gmail.com>
2026-05-12 11:43:59 +0400

    Тук тук

--- Content.Shared/_Stories/Clothing/HelmetTapSystem.cs
+++ Content.Shared/_Stories/Clothing/HelmetTapSystem.cs
@@ -1,6 +1,9 @@
 using Content.Shared._RMC14.Emote;
 using Content.Shared.Chat.Prototypes;
 using Content.Shared.Interaction;
+using Content.Shared.Inventory;
+using Content.Shared.Popups;
+using Content.Shared.Weapons.Ranged.Components;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
@@ -12,6 +15,8 @@ public sealed class HelmetTapSystem : EntitySystem
     [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly SharedRMCEmoteSystem _emote = default!;
+    [Dependency] private readonly InventorySystem _inventory = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
 
     public override void Initialize()
     {
@@ -20,6 +25,10 @@ public sealed class HelmetTapSystem : EntitySystem
 
     private void OnInteractUsing(Entity<HelmetTapComponent> ent, ref InteractUsingEvent args)
     {
+        //проверка на то, что шлем находится на голове (чтобы нельзя было стучать по шлемам на земле)
+        if (!_inventory.TryGetContainingSlot(ent.Owner, out var slot) || slot.Name != "head")
+            return;
+
         var curTime = _timing.CurTime;
 
         if (curTime < ent.Comp.LastTapTime + ent.Comp.Cooldown)
@@ -28,8 +37,27 @@ public sealed class HelmetTapSystem : EntitySystem
         ent.Comp.LastTapTime = curTime;
         Dirty(ent);
 
+        //проигрывание аудио и эмоута в чат
         _audio.PlayPredicted(ent.Comp.TapSound, ent, args.User);
         _emote.TryEmoteWithChat(args.User, "HelmetTap");
+
+        // Локальное сообщение о патронах в магазине
+        if (TryComp<BallisticAmmoProviderComponent>(args.Used, out var ammo))
+        {
+            var percent = (float)ammo.Count / ammo.Capacity * 100f;
+            string msg;
+
+            if (percent >= 100f)
+                msg = "Магазин полон";
+            else if (percent > 50f)
+                msg = "Магазин наполовину полон";
+            else if (percent > 0f)
+                msg = "Магазин наполовину пуст";
+            else
+                msg = "Магазин пуст";
+
+            _popup.PopupClient(msg, args.User, args.User, PopupType.Medium);
+        }
 
         args.Handled = true;
     }
