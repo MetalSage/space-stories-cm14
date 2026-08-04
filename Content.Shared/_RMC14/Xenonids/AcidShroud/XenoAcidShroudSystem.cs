14ecadcccb2d2eae4abb554da4f712e2d44ee1b0
drunkmaster <137797187+drunkmaster-dev@users.noreply.github.com>
2026-08-04 17:25:13 +0700

    Update XenoAcidShroudSystem.cs

--- Content.Shared/_RMC14/Xenonids/AcidShroud/XenoAcidShroudSystem.cs
+++ Content.Shared/_RMC14/Xenonids/AcidShroud/XenoAcidShroudSystem.cs
@@ -25,8 +25,7 @@ public sealed class XenoAcidShroudSystem : EntitySystem
     {
         args.Handled = true;
         var ev = new XenoAcidShroudDoAfterEvent();
-        var doAfter = new DoAfterArgs(EntityManager, ent, ent.Comp.DoAfter, ev, ent, args.Action);
-        // Stories-Boiler update
+        var doAfter = new DoAfterArgs(EntityManager, ent, ent.Comp.DoAfter, ev, ent, args.Action); // Stories-Boiler
         _doAfter.TryStartDoAfter(doAfter);
 
         _rmcActions.DisableSharedCooldownEvents(args.Action.Owner, ent);
